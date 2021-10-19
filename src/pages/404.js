de9807003d9f08fe079abeec50e16e0d5f2c163f
Navdeep singh <navsingh23@WKMZT0526580.global.publicisgroupe.net>
2021-10-19 11:35:55 +0530

    Updaten

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,22 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { Helmet } from "react-helmet"
 
 const NotFound = () => {
-    return (
-        <Layout>
-            <Helmet>
-                <title>Navdeep Singh - Full Stack Developer</title>
-                <meta name="description" content="Welcome to my personal website." />
-                <meta property="og:type" content="website" />
-            </Helmet>
-            <section className="home">
-                <div className="introduction" id="main">
-                    <h1>404: This was probably a mistake.</h1>
-                </div>
-            </section>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Helmet>
+        <title>Navdeep Singh - Full Stack Developer</title>
+        <meta name="description" content="Welcome to my personal website." />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      <section className="home">
+        <div className="introduction" id="main">
+          <h1>404: This was probably a mistake.</h1>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
-export default NotFound;
+export default NotFound
